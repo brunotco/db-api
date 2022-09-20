94ea4fc908120721b35cfc9242cffe6998432cc5
brunotco <brunotco4@gmail.com>
2022-09-20 10:59:00 +0100

    Updated modules imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { ProductModule } from './product/product.module';
+import { AuthModule } from '@modules/auth/auth.module';
+import { ProductModule } from '@modules/product/product.module';
 
 @Module({
   imports: [
