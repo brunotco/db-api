391c18d050d0c3581d2a3ffab2af42af55b1f46c
brunotco <brunotco4@gmail.com>
2022-09-20 13:20:09 +0100

    Fixed entity files not being found.

--- src/common/config/typeorm.config.ts
+++ src/common/config/typeorm.config.ts
@@ -3,6 +3,6 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: "sqlite",
     database: "productsDB",
-    entities: [__dirname + "/**/*.entity{.ts,.js}"],
+    entities: [__dirname + "/../../**/*.entity{.ts,.js}"],
     synchronize: true
 };
