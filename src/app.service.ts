40fc57fcce8ebe1b8a2a74f51fa7f8d3d7e62e80
brunotco <brunotco4@gmail.com>
2022-09-20 14:25:20 +0100

    Load env file from a common folder.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Injectable()
 export class AppService {
+  
+  // constructor(private config: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
+
+  // appOrmConfig(): TypeOrmModuleOptions {
+  //   const type = this.config.get('DB_TYPE');
+  //   const database = this.config.get('DB_NAME')
+  //   console.log(type, database)
+  //   return {
+  //       type: type,
+  //       database: database,
+  //       entities: [__dirname + "**/*.entity{.ts,.js}"],
+  //       synchronize: true
+  //   }
+  // }
+
+  // static appTypeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  //   useFactory: async (): Promise<TypeOrmModuleOptions> => {
+  //       return this.appOrmConfig();
+  //   }
+  // };
 }
