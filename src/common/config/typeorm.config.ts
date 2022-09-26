14ced118e4fd20c6da3310df2d1f8dabb4919e36
brunotco <brunotco4@gmail.com>
2022-09-26 10:03:06 +0100

    Not needed join path for entities.

--- src/common/config/typeorm.config.ts
+++ src/common/config/typeorm.config.ts
@@ -1,12 +1,10 @@
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
-import { join } from 'path';
 
 export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
   useFactory: () => ({
     type: process.env.DB_TYPE as any,
     database: process.env.DB_NAME as any,
-    // entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-    entities: [join(__dirname, '../../entities/**', '*.entity{.ts,.js}')],
+    entities: [__dirname + '/../../entities/**/*.entity{.ts,.js}'],
     synchronize: true,
   }),
 };
